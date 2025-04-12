247c8ab4bba3c37cc830329322e1556ead6fe617
sijanthapaa34 <sijanmgr034@gmail.com>
2025-04-12 17:09:53 +0545

    change in app

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ajan-thapa-portfolio-nexus">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
