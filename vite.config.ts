cb7f7d8e5cec91c6c10e2f7cbdbdfb14a9a293bc
sijanthapaa34 <sijanmgr034@gmail.com>
2025-04-13 19:34:41 +0545

    change in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ajan-thapa-portfolio-nexus/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
